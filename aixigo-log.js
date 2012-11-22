e2d55945d6bbf076c55971ab9be9ecce8345f6ab
Dirk Ecker <Dirk.Ecker@aixigo.de>
2012-11-22 16:57:35 +0100

    handle more than one argument to log, renamed main to aixigo-log

---
+++ aixigo-log.js
@@ -0,0 +1,106 @@
+
+(function ( name, global, definition ) {
+
+   'use strict';
+
+   if( typeof module === 'object' && typeof require === 'function' ) {
+      module.exports = definition( name, global ); /* commonJS */
+   } else if( typeof define === 'function' && typeof define.amd === 'object' ) {
+      define( definition ); /* AMD */
+   } else {
+      global[ name ] = definition( name, global );
+   }
+
+} ( 'Log', ( typeof window !== 'undefined' && window ) || this, function definition( name, global ) {
+
+   'use strict';
+
+   // logger object that will be returned as the API
+   var logger = {},
+
+   // regular expression to extract file, function and line number from stack trace
+    //var findDetailsRegularExpression = /^\s*at\s*([\w<>\.]*)\s*\((.*?):(\d*):(\d*)\)$/; extracts line number and column
+      findDetailsRegularExpression = /^\s*at\s*([\w<>\.]*)\s*(.*)$/,
+
+   // all available log level
+      allLogLevel = [
+         'NONE',
+         'ERROR',
+         'WARN',
+         'INFO',
+         'DEBUG',
+         'TRACE',
+         'DATA',
+         'DEVELOP'
+      ],
+
+   // initial log level used
+      actualLogLevel = 3, //INFO
+
+   // log method to consume logging messages
+      actualConsumerFunction = console.log.bind( console),
+
+   // creates the api to log messages
+      currentLevel = null;
+   var allLogLevelLength = allLogLevel.length;
+   for ( currentLevel = 0; currentLevel < allLogLevelLength; currentLevel += 1 ) {
+      if( allLogLevel[ currentLevel ] === 'NONE' ) { continue; }
+      logger[ allLogLevel[ currentLevel ].toLowerCase() ] = function ( currentLevel ) {
+          return function () {
+              if ( actualLogLevel < currentLevel ) { return; }
+             log( Array.prototype.join.call( arguments, ' ') );
+          };
+      } ( currentLevel );
+   }
+
+   // set consumer of the log messages
+   function to( consumer ) {
+      actualConsumerFunction = consumer;
+   }
+
+   // the function that logs using an error
+   function log( message ) {
+       var stackInfo = ( new Error() ).stack.split( '\n' )[ 3 ];
+       var matches = findDetailsRegularExpression.exec( stackInfo );
+       var callingFunction = null;
+       var callingOccured = null;
+       var logMessage = null;
+
+       if( matches === null ) {
+          console.log( stackInfo );
+          throw new Error( 'InvalidStackError', 'Stacktrace is invalid' );
+       } else {
+          callingFunction = matches[ 1 ];
+          callingOccured = matches[ 2 ];
+
+          logMessage = new Date().toISOString() + ': ' + message + ' ' + callingOccured + ' in ' + callingFunction;
+          actualConsumerFunction.call( this, logMessage );
+       }
+   }
+
+   // set the actual log level or returns it
+   function level( newLevel ) {
+      if( newLevel ) {
+         actualLogLevel = allLogLevel.indexOf( newLevel );
+      } else {
+         return allLogLevel[ actualLogLevel ];
+      }
+   }
+
+   // API
+   return {
+      to:      to,
+      //all:     allLogLevel,
+      level:   level,
+
+      error:   logger.error,
+      warn:    logger.warn,
+      info:    logger.info,
+      debug:   logger.debug,
+      trace:   logger.trace,
+      data:    logger.data,
+      develop: logger.develop
+
+   };
+
+} ) );
--- spec/LogSpec.js
+++ spec/LogSpec.js
@@ -6,31 +6,31 @@
       var lastLogMessage = null;
       var logConsumer = function consume( message ) { lastLogMessage = message; };
       Log.to( logConsumer );
-      console.log( Log );
+      //console.log( Log );
 
       it( "has log level INFO as default.", function() {
          expect( Log.level() ).toEqual( 'INFO' );
       });
 
-      it( "logs Hello", function() {
+      it( "logs Hello.", function() {
          Log.level( 'DEVELOP' );
          Log.develop( 'Hello' );
          expect( lastLogMessage ).toMatch( 'Hello' );
       });
 
-       it( "does not log debug messages if run on level INFO", function() {
+       it( "does not log debug messages if run on level INFO.", function() {
            Log.level( 'INFO' );
            Log.debug( 'not logged' );
            expect( lastLogMessage ).not.toMatch( 'not logged' );
        });
 
-       it( "does log error messages if run on level INFO", function() {
+       it( "does log error messages if run on level INFO.", function() {
            Log.level( 'INFO' );
            Log.error( 'logged' );
            expect( lastLogMessage ).toMatch( 'logged' );
        });
 
-       it( "can be set to log level WARN", function() {
+       it( "can be set to log level WARN.", function() {
            Log.level( 'WARN' );
            expect( Log.level() ).toEqual( 'WARN' );
        });
@@ -42,6 +42,14 @@
            // back to internal consumer
            expect( function () { Log.to( logConsumer ); } ).not.toThrow( 'TypeError: Illegal invocation' );
        });
+
+       it( "can handle more than one argument.", function() {
+           Log.level( 'INFO' );
+           Log.info( 'Hello', 'You', 123 );
+           expect( lastLogMessage ).toMatch( 'Hello You 123' );
+       });
+
+
 
    });
 
